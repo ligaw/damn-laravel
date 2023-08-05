a80f9883b1e894ca8bdd4b673a4bce9664badb25
Pablo Puyat <pablo@cbrz.com>
2023-08-05 14:47:24 -0400

    Installed Laravel Folio Basic global styling Basic styling on login and register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,50 @@
 <x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+    <x-slot:header>
+        Sign in
+    </x-slot:header>
+    @if (session('status'))
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ session('status') }}
+        </div>
+    @endif
 
-        <x-validation-errors class="mb-4" />
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
+        <div>
+            <x-label for="email" value="{{ __('Email') }}"/>
+            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                     autofocus autocomplete="username"/>
+        </div>
+
+        <div>
+            <x-label for="password" value="{{ __('Password') }}"/>
+            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                     autocomplete="current-password"/>
+        </div>
+
+        <div>
+            <label for="remember_me" class="flex items-center">
+                <x-checkbox id="remember_me" name="remember"/>
+                <span class="text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div>
+
+
+            <x-button>
+                {{ __('SEND') }}
+            </x-button>
+        </div>
+    </form>
+    <x-validation-errors class="mb-4"/>
+    <x-slot:footer>
+        @if (Route::has('password.request'))
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+               href="{{ route('password.request') }}">
+                {{ __('Forgot your password?') }}
+            </a>
         @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
+    </x-slot:footer>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,22 +1,70 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
+    <style>
+        body, html {
+            height: 100%;
+            margin: 0;
+        }
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
-    </body>
+        .container {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh; /* Ensure container is at least the height of the viewport */
+            margin: 0 auto;
+        }
+
+        .container > .third {
+            /*border: 1px solid black;*/
+            padding: 1rem 0;
+            flex: 1; /* Distribute equal space among children */
+        }
+
+        header {
+            align-items: start;
+            display: flex;
+            font-size: 3rem;
+        }
+
+        div.third {
+            align-items: start;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            font-family: 'Inter', sans-serif;
+        }
+
+        footer {
+            align-items: end;
+            display: flex;
+        }
+    </style>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<div class="container">
+    <header class="third">
+        {{ $header }}
+    </header>
+    <div class="third">
+        {{ $slot }}
+    </div>
+    <footer class="third">
+        {{ $footer }}
+    </footer>
+</div>
+</body>
 </html>
